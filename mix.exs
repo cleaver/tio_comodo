59a55052c17b6e61c69a0517325e83115b56419c
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2025-08-18 12:10:42 -0400

    remove boilerplate

--- mix.exs
+++ mix.exs
@@ -6,16 +6,7 @@ defmodule TioComodo.MixProject do
       app: :tio_comodo,
       version: "0.1.0",
       elixir: "~> 1.18",
-      start_permanent: Mix.env() == :prod,
       deps: deps()
-    ]
-  end
-
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger],
-      mod: {TioComodo.Application, []}
     ]
   end
 
--- test/tio_comodo_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TioComodoTest do
-  use ExUnit.Case
-  doctest TioComodo
-
-  test "greets the world" do
-    assert TioComodo.hello() == :world
-  end
-end
